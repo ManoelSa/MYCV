fc5c723ea903f43e6ccf4410faf48ec448a5b020
Manoel Sa <mmessias.sa@gmail.com>
2021-06-15 21:46:24 -0300

    Mudança na estrutura

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-$(document).ready(function(){
-    $("#exp_add").click(function(){
-        $(".timeline").toggle(1000);
-        $('#exp_prof').children('h2').remove(); 
-        $("#exp_prof").append("<h2><i id='exp_rem' class='material-icons' style='cursor: pointer;'>remove_circle</i> Experiência Profissional</h2>");
+$(document).ready(function () {
+    $('#expprof').click(function () {
+        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.timeline').toggle(1000);
+
     });
 });
 
-$(document).ready(function(){
-    $(".perfprof").click(function(){
-        $(".timeline").hide(1000);
-        $('#exp_prof').children('h2').remove(); 
-        $("#exp_prof").append("<h2><i id='exp_add' class='material-icons' style='cursor: pointer;'>add_circle</i> Experiência Profissional</h2>");
+
+$(document).ready(function () {
+    $('#foracad').click(function () {
+        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.formaca').toggleClass('geral',1000);    
+    });
+});
+
+$(document).ready(function () {
+    $('#forcomp').click(function () {
+        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.formcom').toggleClass("geral");
+     });
+});
+
+
+$(document).ready(function () {
+    $('#certf').click(function () {
+        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.certifi').toggleClass("geral");
+
     });
 });
