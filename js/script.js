2abd3ee3a207dcbb4443a343deb58008b0c558ad
Manoel Sa <mmessias.sa@gmail.com>
2021-06-16 19:29:07 -0300

    Avançando na estrutura

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-$(document).ready(function () {
-    $('#expprof').click(function () {
-        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+function recebe_id(parametro){
+    let id_retorno = '#'+ parametro
+
+    if(id_retorno == '#expprof'){
+        $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
         $('.timeline').toggle(1000);
+    }else if (id_retorno == '#foracad'){
+        $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+       // $('.formaca').toggleClass('geral',1000); 
+       $('.formaca').toggle(1000);
+    }else if (id_retorno == '#forcomp'){
+        $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.formcom').toggle(1000);
+    }else if (id_retorno == '#certf'){
+        $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
+        $('.certifi').toggle(1000);
+    }else{
+        console.log('Sem Registro')
+    }
 
-    });
-});
-
-
-$(document).ready(function () {
-    $('#foracad').click(function () {
-        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-        $('.formaca').toggleClass('geral',1000);    
-    });
-});
-
-$(document).ready(function () {
-    $('#forcomp').click(function () {
-        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-        $('.formcom').toggleClass("geral");
-     });
-});
+}
 
 
 $(document).ready(function () {
-    $('#certf').click(function () {
-        $(this).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-        $('.certifi').toggleClass("geral");
+    $('h2 i').click(function(){
+    let id_h2 = $(this).parent().attr('id')
+        recebe_id(id_h2)
+    })
 
-    });
 });
