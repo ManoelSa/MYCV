7c064877573ea6fc891fe069e578d01d0a33d1fa
Manoel Sa <mmessias.sa@gmail.com>
2021-06-17 16:32:29 -0300

    Atualizando scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+//Efeito
 function recebe_id(parametro){
     let id_retorno = '#'+ parametro
 
@@ -24,7 +26,39 @@ function recebe_id(parametro){
 $(document).ready(function () {
     $('h2 i').click(function(){
     let id_h2 = $(this).parent().attr('id')
-        recebe_id(id_h2)
-    })
+        recebe_id(id_h2)     
+    })     
+});
 
+
+//Scroll
+function goTo (element, speed){
+	let id_h2 = '#'+element.attr('id');
+	let top = $(id_h2).offset().top;
+    $("html, body").animate({scrollTop: top},speed)
+}
+
+$(function(){
+	$("h2").click(function(e){
+	e.preventDefault();
+	goTo($(this),1000);
+ });
+});
+
+
+$(document).ready(function() {
+    // Exibe ou oculta o botão
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.voltar-ao-topo').fadeIn(200);
+        } else {
+            $('.voltar-ao-topo').fadeOut(200);
+        }
+    });
+    
+    // Faz animação para subir
+    $('.voltar-ao-topo').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1500);
+    })
 });
