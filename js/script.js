32f625620517f96cdde5d825b528cee8d8f40a3b
Manoel Sa <mmessias.sa@gmail.com>
2021-06-18 00:06:58 -0300

    Inclusao de IMpressao

--- js/script.js
+++ js/script.js
@@ -1,48 +1,49 @@
-
 //Efeito
-function recebe_id(parametro){
-    let id_retorno = '#'+ parametro
+function recebe_id(parametro) {
+    let id_retorno = '#' + parametro
 
-    if(id_retorno == '#expprof'){
+    if (id_retorno == '#expprof') {
         $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
         $('.timeline').toggle(1000);
-    }else if (id_retorno == '#foracad'){
+    } else if (id_retorno == '#foracad') {
         $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-       // $('.formaca').toggleClass('geral',1000); 
-       $('.formaca').toggle(1000);
-    }else if (id_retorno == '#forcomp'){
+        $('.formaca').toggleClass('ocultar geral');
+        //$('.formaca').toggle(1000);
+    } else if (id_retorno == '#forcomp') {
         $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-        $('.formcom').toggle(1000);
-    }else if (id_retorno == '#certf'){
+        //$('.formcom').toggle(1000);
+        $('.formcom').toggleClass('ocultar geral');
+    } else if (id_retorno == '#certf') {
         $(id_retorno).find('i').toggleClass('fa-minus-circle fa-plus-circle');
-        $('.certifi').toggle(1000);
-    }else{
+        //$('.certifi').toggle(1000);
+        $('.certifi').toggleClass('ocultar geral');
+    } else {
         console.log('Sem Registro')
     }
 
 }
 
 
-$(document).ready(function () {
-    $('h2 i').click(function(){
-    let id_h2 = $(this).parent().attr('id')
-        recebe_id(id_h2)     
-    })     
+$(document).ready(function() {
+    $('h2 i').click(function() {
+        let id_h2 = $(this).parent().attr('id')
+        recebe_id(id_h2)
+    })
 });
 
 
 //Scroll
-function goTo (element, speed){
-	let id_h2 = '#'+element.attr('id');
-	let top = $(id_h2).offset().top;
-    $("html, body").animate({scrollTop: top},speed)
+function goTo(element, speed) {
+    let id_h2 = '#' + element.attr('id');
+    let top = $(id_h2).offset().top;
+    $("html, body").animate({ scrollTop: top }, speed)
 }
 
-$(function(){
-	$("h2").click(function(e){
-	e.preventDefault();
-	goTo($(this),1000);
- });
+$(function() {
+    $("h2").click(function(e) {
+        e.preventDefault();
+        goTo($(this), 1000);
+    });
 });
 
 
@@ -55,10 +56,10 @@ $(document).ready(function() {
             $('.voltar-ao-topo').fadeOut(200);
         }
     });
-    
+
     // Faz animação para subir
     $('.voltar-ao-topo').click(function(event) {
         event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 1500);
+        $('html, body').animate({ scrollTop: 0 }, 1500);
     })
 });
